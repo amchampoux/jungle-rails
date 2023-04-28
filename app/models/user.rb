871f64ce1b73ca9d84e97d57ee80a9ae763ea1ed
amchampoux <amchampoux@mirego.com>
2023-04-28 17:34:32 -0400

    add validation on account creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
 
   has_secure_password
+
+  validates :first_name, presence: { message: "must be given please" }
+  # validates :last_name, presence: true
+  # validates :email, presence: true
+  # validates :password, presence: true
+  # validates :password_confirmation, presence: true
+  # validates :email { case_sensitive: true }
+
   
 end
+
+
+
